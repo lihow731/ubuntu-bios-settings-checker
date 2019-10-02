950eca09cfc7304dd88b7e2a15f2f19401abecb2
Leon Liao <lihow731@gmail.com>
2019-10-02 19:01:14 +0800

    Implement UbuntuBIOSSettingsChecker and DellChecker class

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
 # ubuntu-bios-settings-checker
 Provide a API to check BIOS setting for Ubuntu installer
+
+# Usage
+## UbuntuBIOSSettingsChecker class
+This class will provide an abstract level. 
+According the manufacter (read from DMI) to load the manufacter class.
+### Syntax
+
+```
+class UbuntuBIOSSettingsChecker:
+   def __init__(self, configFile = "" )
+```
+
+####Parameters
+##### configFile
+The full file patch of the configure file.
+If you don't set it. The default configure file patch is
+/usr/share/UbuntuBIOSSettingsChecker/config.
+
+####member
+#####status
+######0: class is initialized successfully.
+
+######-1: There are something wrong. Maybe the manufacturer is not support yet.
+
+#####support:
+
+######True: The BIOS settings is proper.
+
+######False: The BIOS settings is not proper.
+
+
+## DellChecker class
+Using the libsmbios to check the BIOS settings.
+### Syntax
+
+```
+class DellChecker():
+	configure = None
+	def __init__(self, configFile = "")
+	def check(self)
+	def readConfigure(self, configFile = "")
+	def tokenInfo(self, tokenObj, action)
+```
+
+####Parameters
+#####configFile
+The full file patch of the configure file.
+If you don't set it. The default configure file patch is
+/usr/share/UbuntuBIOSSettingsChecker/config.
+
+#####tokenObj
+The tokenObj come from smbios-token-ctl.
+
+```
+tokenTable = smbios_token.TokenTable()
+tokenObj = tokenTable[smbios_keys]
+```
+
+#####action
+This parameter come from smbios-token-ctl.
+
+"is-bool": the method wil return the value of a bool.
+
+"is-string": the method wil return the value of a string.
+
+"is-active": the method wil return the active state
+
+
+### Example 
+
+```
+import UbuntuBIOSSettingsChecker
+def main():
+    ubsc=UbuntuBIOSSettingsChecker("config")
+    if ubsc.status == 0:
+        if ubsc.support :
+            print("Ubuntu can be installed with this BIOS settings.")
+        else:
+            print("The BIOS settings is not proper.")
+    else:
+        print("This utility does not support {} yet.".format(ubsc.manufacturer))
+
+```
+
+## Comand line
+### Syntax
+
+```
+sudo python3 UbuntuBIOSSettingsChecker.py 
+```
--- UbuntuBIOSSettingsChecker.py
+++ UbuntuBIOSSettingsChecker.py
@@ -1,8 +1,128 @@
-class UbuntuBIOSSettingsChecker:
+#!/usr/bin/env python3
+import os
+import subprocess
+import sys
+import json
+from libsmbios_c import smbios_token
+
+DEFAULT_CONFIGURE_PATH="/usr/share/UbuntuBIOSSettingsChecker/config"
+
+class CheckerTemplator:
     def __init__(self):
         pass
+    def readConfigure():
+        pass
+
+class DellChecker(CheckerTemplator):
+    configure = None
+    tokenTable = smbios_token.TokenTable()
+    def __init__(self, configFile = ""):
+        self.readConfigure(configFile)
+        pass
+
+    def check(self):
+        ret = True
+        try:
+            productName = subprocess.Popen("dmidecode -s system-product-name".split(),stdout=subprocess.PIPE ).stdout.read().strip().decode("utf-8")
+        except(e):
+            print("{}".format(e))
+            pass
+        
+        # check the default settings
+        print(self.configure["default"])
+        for key in self.configure["default"].keys():
+            if key != "url":
+                tokenObj = self.tokenTable[int(key)]
+                if type(self.configure["default"][key]["value"]) == bool:
+                    (exit_code, t, value) = self.tokenInfo(tokenObj, "is-bool" )
+                else:
+                    (exit_code, t, value) = self.tokenInfo(tokenObj, "is-string" )
+                
+                if self.configure["default"][key]["value"] != value:
+                    
+                    return False
+
+        if productName in self.configure.keys():
+            print(self.configure[productName])
+
+        return True
+
+    def readConfigure(self, configFile = ""):
+        configFilePath = None
+        if os.path.isfile(configFile):
+            configFilePath = configFile
+            if os.path.isfile(DEFAULT_CONFIGURE_PATH):
+                configFilePath = DEFAULT_CONFIGURE_PATH
+        if configFilePath:
+            with open(configFilePath) as f:
+                self.configure = json.load(f)
+
+    def tokenInfo(self, tokenObj, action):
+        '''
+        I copy this function from smbios-token-ctl that come from smbios-utils package.
+        '''
+        exit_code=1
+
+        type = _("<weird unknown type>")
+        value = _("<unknown value>")
+        if tokenObj.isBool():
+            if action == "is-bool": exit_code = 0
+            type="bool"
+            value=False
+            if tokenObj.isActive():
+                if action == "is-active": exit_code = 0
+                value=True
+
+        elif tokenObj.isString():
+            if action == "is-string": exit_code = 0
+            type = "string"
+            value = tokenObj.getString()
+
+        return (exit_code, type, value)
+
+
+class UbuntuBIOSSettingsChecker:
+    support = False
+    manufacturer = None
+    status = 0
+    configure = None
+    tokenObj = None
+    checker = None
+    def __init__(self, configFile = "" ):
+        try:
+            manufacturer = subprocess.Popen("dmidecode -s system-manufacturer".split(),stdout=subprocess.PIPE ).stdout.read().strip()
+        except(FileNotFoundError):
+            self.status = -1
+            return None
+    
+        print("The manufacturer is {}.".format(manufacturer))
+        self.manufacturer = manufacturer
+
+        if manufacturer == b"Dell Inc." :
+            self.checker = DellChecker(configFile)
+        elif manufacturer == b"Hewlett-Packard":
+            #self.checker = HPChecker()
+            print("This class don't support {} yet.".format(manufacturer))
+            return None
+        else :
+            print("This class don't support {} yet.".format(manufacturer))
+            self.status = -1
+            return None
+        
+        self.support = self.checker.check()
+
 
 def main():
+    ubsc=UbuntuBIOSSettingsChecker("config")
+    if ubsc.status == 0:
+        if ubsc.support :
+            print("Ubuntu can be installed with this BIOS settings.")
+        else:
+            print("The BIOS settings is not proper.")
+    else:
+        print("This utility does not support {} yet.".format(ubsc.manufacturer))
+
+
     pass
 
 if __name__ == "__main__":
